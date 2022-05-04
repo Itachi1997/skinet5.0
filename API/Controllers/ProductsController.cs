fc1c7844eafa9bd79589f2c1d6fbbe7c8c703d7d
Itachi1997 <waseemfiaz86@gmail.com>
2022-05-05 03:43:53 +0500

    End of section 8

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using API.Controllers.Errors;
 using API.Dtos;
--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './Models/Pagination';
+import { IProduct } from './Models/Product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: IProduct[] = [];
+
+  constructor(private httpclient: HttpClient) { }
+
+  ngOnInit(): void {
+    this.httpclient.get("https://localhost:5001/api/products?pageSize=50").subscribe({
+      next: (res: IPagination) => {
+        this.products = res.data;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+    // ((res: IPagination) => {
+    //   this.products = res.data;
+    // }
+    // , err => {
+    //   console.log(err);
+    // }
+    // );
+  }
 }
